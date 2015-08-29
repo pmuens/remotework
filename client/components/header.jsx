376f2a7c01ed9421a7685706c34c056e52632aed
Philipp Muens <raute1337@gmx.de>
2015-08-29 09:56:44 +0200

    Add link to root-Path in headers headline
    Add link to root-Path in headers headline so that the user can
    navigate to the root-Path if necessary.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,7 +6,9 @@ Header = React.createClass({
           <div className="row">
             <div className="twelve columns">
               <div className="text-center">
-                <h1>Remote Work</h1>
+                <h1>
+                  <a href="/">Remote Work</a>
+                </h1>
                 <p>Job board for everything that can be done remotely</p>
                 <a className="button" href="#">Add job offer</a>
               </div>
