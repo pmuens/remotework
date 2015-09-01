85345d37702379f182bdeba729458a233cb602e7
Philipp Muens <raute1337@gmx.de>
2015-09-01 15:53:41 +0200

    Add load more jobs functionality
    Add load more jobs functionality so that only 20 jobs are displayed at the
    beginning but more jobs can be loaded with a click on the load more button.
    This reduces bandwidth and load-Time.

--- client/components/jobs/list.jsx
+++ client/components/jobs/list.jsx
@@ -1,10 +1,25 @@
 JobsList = React.createClass({
   mixins: [ReactMeteorData],
 
+  getInitialState() {
+    return { jobLimit: parseInt(this.props.limit) || this.increment() }
+  },
+
   getMeteorData() {
+    let jobsSubscription =  Meteor.subscribe('jobs', this.state.jobLimit);
     return {
-      subscriptions: Meteor.subscribe('jobs'),
+      subscription: jobsSubscription,
       jobs: Jobs.find({}, { sort: { createdAt: -1 } }).fetch()
+    }
+  },
+
+  increment() {
+    return 20;
+  },
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.filter !== this.props.filter) {
+      this.setState({jobLimit: nextProps.limit})
     }
   },
 
@@ -12,6 +27,16 @@ JobsList = React.createClass({
     return _.map(this.data.jobs, (job) => {
       return <JobItem job={job}/>;
     });
+  },
+
+  loadMore() {
+    if(Jobs.find({}, { limit: this.jobLimit }).count() === this.state.jobLimit) {
+      return <a className="button u-full-width" onClick={this.onLoadMore}>Load more</a>
+    }
+  },
+
+  onLoadMore() {
+    this.setState({ jobLimit: this.state.jobLimit + this.increment() })
   },
 
   render() {
@@ -23,6 +48,11 @@ JobsList = React.createClass({
               {this.jobItems().length ? (
                 this.jobItems()
               ) : <div className="alert info">There are no jobs available to display</div> }
+            </div>
+          </div>
+          <div className="row">
+            <div className="twelve columns">
+              {this.loadMore()}
             </div>
           </div>
         </div>
--- server/publications.jsx
+++ server/publications.jsx
@@ -1,9 +1,12 @@
-Meteor.publish('jobs', () => {
+Meteor.publish('jobs', (limit) => {
+  check(limit, Number);
+
   return Jobs.find(
     {},
     {
       sort: { createdAt: -1 },
-      fields: { title: true, description: true, email: true, company: true, homepage: true, createdAt: true }
+      fields: { title: true, description: true, email: true, company: true, homepage: true, createdAt: true },
+      limit: limit
     }
   );
 });
